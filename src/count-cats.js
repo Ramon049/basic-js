5a2ae3a5580f45b8818c6343ccd41d3c7bc6b8f6
Roman Koshla <koshla.roman10@gmail.com>
2020-02-18 01:35:04 +0400

    feat: add solution for count-cats, dream-ream and transform-array

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(cats) {
+  let res = 0;
+
+  for(let i = 0; i < cats.length; i++){
+    for(let j = 0; j < cats[i].length; j++){
+      if(cats[i][j] == "^^"){
+        res++;
+      }
+    }
+  }
+  return res;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(team) {
+  if(!Array.isArray(team)){
+    return false
+  }
+  let res = [];
+
+  for(let i = 0, j = 0; i < team.length; i++){
+    if(typeof team[i] !== 'string'){
+      continue;
+    }
+      res.push(team[i].trim()[j].toUpperCase());
+  }
+  return res.sort().join('');
 };
