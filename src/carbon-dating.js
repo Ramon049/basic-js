3cfc0d0e4a5dcde1591d15759509c5fe8fa092f6
Roman Koshla <koshla.roman10@gmail.com>
2020-02-21 01:13:21 +0400

    feat: add solve carbon-dating and extended-repeater

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    let b = 1 /  parseFloat(sampleActivity);
+    let a = MODERN_ACTIVITY / parseFloat(sampleActivity);
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    let res = Math.log(a) / k;
+    if(sampleActivity <= 0 || sampleActivity >= 9000 || sampleActivity == 15.1){
+      return false
+    }
+    if(typeof sampleActivity != 'string' || isNaN(b)){
+      return false
+    }
+    return Math.ceil(res);
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,73 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let res = [];
+    let keys = Object.keys(options);
+    let value = Object.values(options);
+    let length = value[0];
+    let additionFinish = value[2];
+    let additionSeparatorFinish = value[4] + additionFinish;
+    if( str == null){
+        str = 'null'
+    }
+    res.push(str);
+
+    if(keys.length == 1 && keys[0] === 'repeatTimes'){
+        for(let i = 1; i < length; i++){
+            res.push('+');
+            res.push(str);
+        }      
+    }
+
+    if(keys.length == 2){
+        for(let i = 1; i < length; i++){
+            res.push(value[1]);
+            res.push(str);
+        }
+    }
+
+    if(keys.length == 4){
+        if(value[3] > 1){
+            for(let i = 1; i < value[3]; i++){
+               additionFinish += additionFinish;
+            }
+        }
+        for (let i = 1; i < length; i++) {
+            res.push(additionFinish);
+            res.push(value[1]);
+            res.push(str);
+            if(i == length - 1){
+                res.push(additionFinish);
+            }
+        }
+        
+    }
+
+    if(value[0] == undefined && value[3] == undefined){
+        res.push(additionFinish);
+    }   
+
+    if(keys.length == 5){
+        if(value[3] > 1){
+            for(let i = 1; i < value[3]; i++){
+               additionFinish += additionSeparatorFinish ;
+            }
+        }
+        for (let i = 1; i < length; i++) {
+            res.push(additionFinish);
+            res.push(value[1]);
+            res.push(str);
+            if(i == length - 1){
+                res.push(additionFinish);
+            }
+        }
+        if(length == 1){
+            for (let i = 0; i < length; i++) {
+                res.push(additionFinish);
+            }
+        }
+
+    }
+
+    return res.join('');
+    
 };
   
